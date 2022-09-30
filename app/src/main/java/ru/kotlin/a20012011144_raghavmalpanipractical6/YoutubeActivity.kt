85bc1e672fbce33fce470825f7b6c97e81aae9b6
rmalp <malpaniraghav65@gmail.com>
2022-09-30 11:04:42 +0500

    Pr6

--- app/src/main/java/ru/kotlin/a20012011144_raghavmalpanipractical6/YoutubeActivity.kt
+++ app/src/main/java/ru/kotlin/a20012011144_raghavmalpanipractical6/YoutubeActivity.kt
@@ -1,8 +1,10 @@
 package ru.kotlin.a20012011144_raghavmalpanipractical6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class YoutubeActivity : AppCompatActivity() {
     private val youtubeId="MN8p-Vrn6G0"
@@ -16,5 +18,9 @@ class YoutubeActivity : AppCompatActivity() {
         settings.useWideViewPort=true
 
         mywebview.loadUrl("https://www.youtube.com/embed/$youtubeId")
+        val button=findViewById<FloatingActionButton>(R.id.webfloat)
+        button.setOnClickListener{
+            Intent(this,MainActivity::class.java).apply{startActivity(this)}
     }
+}
 }
